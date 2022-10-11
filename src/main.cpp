79860960c90d4159dff709e9f13a0e9a96866dbf
SebJohansen8763 <113422792+SebJohansen8763@users.noreply.github.com>
2022-10-11 12:24:48 +0200

    s

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,3 @@ int main(int argc, char **argv)
 {
     return 0;
 }
-
-void printout(node *l) {
-/*pre: head points to the first, empty element.
-The last element’s next is NULL
-post: the values of the list are printed out*/
-node *p = l->next;
-while (p!=NULL){
-printf("\%d, ",p->data);
-}
-printf("\n");
-}
